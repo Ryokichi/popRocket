b910a46514d5e105a32ca113db3cd1184baaead1
Heitor Ryokichi Nakamura <heitor.nakamura@hotmail.com>
2018-10-16 22:23:51 -0300

    primeiro commit

---
+++ desktop/src/interno/poprocket/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package interno.poprocket.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import interno.poprocket.PopRocket;
+import interno.poprocket.PopRocket;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PopRocket(), config);
+		config.title = "PopRocket";
+		config.width = Gdx.graphics.getWidth();
+		config.height = Gdx.graphics.getHeight();
+	}
+}
